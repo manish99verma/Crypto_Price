8fe9e157ad4e93891a49651985349648a53d53ca
Manish Kumar Verma <manish6203993@gmail.com>
2024-01-03 12:00:31 +0530

    Added Caches for Details view.

--- app/src/main/java/com/manish/cryptoprice/presentation/ui/view_models/DetailsViewModel.kt
+++ app/src/main/java/com/manish/cryptoprice/presentation/ui/view_models/DetailsViewModel.kt
@@ -1,7 +1,9 @@
 package com.manish.cryptoprice.presentation.ui.view_models
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
+import com.manish.cryptoprice.data.model.description.CoinDetails
 import com.manish.cryptoprice.domain.use_case.Get24HoursHourlyPricesListUseCase
 import com.manish.cryptoprice.domain.use_case.GetDescriptionUseCase
 
@@ -9,9 +11,21 @@ class DetailsViewModel(
     private val getCoinsDescriptionUseCase: GetDescriptionUseCase,
     private val get24HoursHourlyPricesListUseCase: Get24HoursHourlyPricesListUseCase
 ) : ViewModel() {
-    fun getCoinDetails(id:String) = liveData {
+    companion object{
+        //Caches
+        private val caches = HashMap<String, CoinDetails>()
+    }
+
+    fun getCoinDetails(id: String) = liveData {
+        if (caches.containsKey(id)) {
+            emit(caches[id]!!)
+            Log.d("TAG", "getCoinDetails: Using caches in viewmodel")
+            return@liveData
+        }
+
         getCoinsDescriptionUseCase.execute(id).collect() {
             emit(it)
+            caches[id] = it
         }
     }
 
